2eed8d0d5f243161d4cd015ee6165b24f25051ac
ymail8372 <ymail8372@naver.com>
2023-02-27 22:18:32 +0900

    20230227

--- python/20230225/1920.py
+++ python/20230225/1920.py
@@ -1,20 +1,13 @@
 import sys
 
 N = int(input())
-N_nums = list(map(int, sys.stdin.readline().split()))
+N_nums = set(map(int, sys.stdin.readline().split()))
 M = int(input())
 M_nums = list(map(int, sys.stdin.readline().split()))
-result = []
 
 for i in range(M) :
-	check = 0
-
-	for j in range(len(N_nums)) :
-		if N_nums[j] == M_nums[i] :
-			N_nums.pop(j)
-			check = 1
-			print(1)
-			break
-
-	if check == 0 :
-		print(0)
+    if M_nums[i] in N_nums :
+        print(1)
+    else :
+        print(0)
+        
